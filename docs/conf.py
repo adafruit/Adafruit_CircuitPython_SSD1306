26cb9bfbb11127c0f51721b8250cd4be869924d5
mrmcwethy <mrmcwethy@yahoo.com>
2018-02-19 11:14:35 -0700

    added missing comma

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
     'sphinx.ext.todo',
 ]
 
