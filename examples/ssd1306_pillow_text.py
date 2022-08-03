0faa48640ee081006049e2969de85a1e6ebdcb2e
Md. Siam <md.siam03@gmail.com>
2022-08-03 18:21:06 +0600

    fix: address changes to addr=0x3C

--- examples/ssd1306_pillow_text.py
+++ examples/ssd1306_pillow_text.py
@@ -19,7 +19,7 @@ RESET_PIN = digitalio.DigitalInOut(board.D4)
 
 # Very important... This lets py-gaugette 'know' what pins to use in order to reset the display
 i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3D, reset=RESET_PIN)
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=RESET_PIN)
 
 # Clear display.
 oled.fill(0)
