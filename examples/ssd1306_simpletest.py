984b41516112f56ab6a5c15310cf74ca42af8e94
Md. Siam <md.siam03@gmail.com>
2022-08-03 18:22:26 +0600

    fix: screen size change

--- examples/ssd1306_simpletest.py
+++ examples/ssd1306_simpletest.py
@@ -18,7 +18,7 @@ i2c = busio.I2C(SCL, SDA)
 # Create the SSD1306 OLED class.
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 # Alternatively you can change the I2C address of the device with an addr parameter:
 # display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
 
